4f039f01aa1667d56fa67bd23866978e1e9ef8ee
Majed25 <50779146+Majed25@users.noreply.github.com>
2024-06-20 16:53:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def trigger_webhook():
     match_ids = [match['id'] for match in matches]
     '''Trigger webhook if any new mathces are played '''
     if len(match_ids) != ids_count['ids_count']:
-        response = requests.post('https://fbtfrdashboard.azurewebsites.net/refresh_dashboard')
+        response = requests.post('https://fbtransfersdashboard.azurewebsites.net/refresh_dashboard')
         print(response.status_code)
         if response.status_code == 200:
             print(response.json())
